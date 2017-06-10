d3512b41876d8a978ed9a88c779ba135e3fd2b1a
salaros <salaros@yandex.ru>
2017-06-10 23:13:59 +0300

    js code formatting

--- assets/wp-pagespeed-purge.js
+++ assets/wp-pagespeed-purge.js
@@ -1,7 +1,7 @@
-jQuery( document ).ready( function( $ ) {
+jQuery(document).ready(function ($) {
 
   $('#wp-admin-bar-pagespeed_purge')
-    .click(function(event) {
+    .click(function (event) {
       event.preventDefault();
 
       var spinner = $(this).find('a');
@@ -10,46 +10,45 @@ jQuery( document ).ready( function( $ ) {
       // Purge cache for the entire website
       // More info: https://modpagespeed.com/doc/system#purge_cache
       $.ajax({
-        method: "PURGE",
-        url: document.location.origin + '/*'
-      })
-      .done(function( msg ) {
-        if (msg && msg.indexOf("successful") > -1) {
-          pagespeed_purge_success(spinner);
-        } else {
+          method: "PURGE",
+          url: document.location.origin + '/*'
+        })
+        .done(function (msg) {
+          if (msg && msg.indexOf("successful") > -1) {
+            pagespeed_purge_success(spinner);
+          } else {
+            pagespeed_purge_error(spinner);
+          }
+        })
+        .fail(function () {
           pagespeed_purge_error(spinner);
-        }
-      })
-      .fail(function() {
-        pagespeed_purge_error(spinner);
-      })
-      .always(function() {
-        setTimeout(function() {
-          pagespeed_purge_reset(spinner);
-        }, 750 );
-      });
+        })
+        .always(function () {
+          setTimeout(function () {
+            pagespeed_purge_reset(spinner);
+          }, 750);
+        });
 
       return false;
-    })
-  ;
+    });
 
   function pagespeed_purge_animate(spinner) {
     pagespeed_purge_reset(spinner);
-    spinner.addClass( 'spin' );    
+    spinner.addClass('spin');
   }
 
   function pagespeed_purge_error(spinner) {
     pagespeed_purge_reset(spinner);
-    spinner.addClass( 'error' );
+    spinner.addClass('error');
   }
 
   function pagespeed_purge_success(spinner) {
     pagespeed_purge_reset(spinner);
-    spinner.addClass( 'success' );
+    spinner.addClass('success');
   }
 
   function pagespeed_purge_reset(spinner) {
-    spinner.removeClass( 'spin' ).removeClass( 'success' ).removeClass( 'error' );
+    spinner.removeClass('spin').removeClass('success').removeClass('error');
   }
 
 });
