47a216b52c7008678189f7e08e0657692b6f2eba
salaros <salaros@yandex.ru>
2017-06-10 16:44:27 +0300

    adding the very first implementation

---
+++ assets/admin.js
@@ -0,0 +1,57 @@
+jQuery( document ).ready( function( $ ) {
+
+  $('#wp-admin-bar-pagespeed_purge')
+    .click(function(event) {
+      event.preventDefault();
+
+      var spinner = $(this).find('a');
+      pagespeed_purge_animate(spinner);
+
+      // Purge cache for the entire website
+      // More info: https://modpagespeed.com/doc/system#purge_cache
+      $.ajax({
+        method: "PURGE",
+        url: document.location.origin + '/*'
+      })
+      .done(function( msg ) {
+        if (msg && msg.indexOf("successful") > -1) {
+          console.log('OK: ' + msg);
+          pagespeed_purge_success(spinner);
+        } else {
+          console.log('Failed: ' + msg);
+          pagespeed_purge_error(spinner);
+        }
+      })
+      .fail(function() {
+        pagespeed_purge_error(spinner);
+      })
+      .always(function() {
+        setTimeout(function() {
+          pagespeed_purge_reset(spinner);
+        }, 750 );
+      });
+
+      return false;
+    })
+  ;
+
+  function pagespeed_purge_animate(spinner) {
+    pagespeed_purge_reset(spinner);
+    spinner.addClass( 'spin' );    
+  }
+
+  function pagespeed_purge_error(spinner) {
+    pagespeed_purge_reset(spinner);
+    spinner.addClass( 'error' );
+  }
+
+  function pagespeed_purge_success(spinner) {
+    pagespeed_purge_reset(spinner);
+    spinner.addClass( 'success' );
+  }
+
+  function pagespeed_purge_reset(spinner) {
+    spinner.removeClass( 'spin' ).removeClass( 'success' ).removeClass( 'error' );
+  }
+
+});
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* Plugin Name: PageSpeed Cache Purge
+* Plugin URI:  https://github.com/salaros/wp-pagespeed-purge
+* Description: One-click PageSpeed cache purging using an admin bar button
+* Version:     1.0.0
+* Author:      Zhmayev Yaroslav aka Salaros
+* Author URI:  https://salaros.com
+* License:     MIT
+* License URI: https://opensource.org/licenses/MIT
+*/
+
+defined( 'ABSPATH' ) or die( 'Nope, not accessing this' );
+
+class PageSpeedPurge {
+
+	public function __construct() {
+		//sets the default trading hour days (used by the content type)
+		add_action( 'admin_enqueue_scripts', array( $this, 'embed_admin_assets' ), 101 );
+
+		// Add 'Clear Redis cache' button to the admin bar
+		add_action( 'admin_bar_menu', array( $this, 'add_admin_bar_button' ), 101 );
+	}
+
+	public function embed_admin_assets() {
+		// This is where you can add your CSS/JS entries for wp-admin UI
+		$plugin_url = plugin_dir_url( __FILE__ );
+		wp_enqueue_style( 'admin-styles', sprintf( '%s/assets/admin.css', $plugin_url ) );
+		wp_enqueue_script( 'admin-styles', sprintf( '%s/assets/admin.js', $plugin_url ) );
+	}
+
+	public function add_admin_bar_button( $wp_admin_bar ) {
+		$args = [
+			'id'	=> 'pagespeed_purge',
+			'title' => 'Clear Pagespeed Cache',
+			'href' => '#pagespeed_purge',
+		];
+
+		$wp_admin_bar->add_menu( $args );
+	}
+}
+
+new PageSpeedPurge();
