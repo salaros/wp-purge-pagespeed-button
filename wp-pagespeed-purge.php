13aa4781dbf3c3e4a9221a234f6c4e89626ef946
salaros <salaros@yandex.ru>
2018-04-27 23:42:32 +0300

    fixing some warning reported by PHP Code Sniffer

--- wp-pagespeed-purge.php
+++ wp-pagespeed-purge.php
@@ -20,11 +20,10 @@ class PageSpeedPurge {
 
 	public function __construct() {
 		// Enqueue css and js files
-		if ( ! is_admin() && is_admin_bar_showing() ) {
-			add_action( 'wp_enqueue_scripts', array( $this, 'embed_admin_assets' ), 101 );
-		} else {
-			add_action( 'admin_enqueue_scripts', array( $this, 'embed_admin_assets' ), 101 );
-		}
+		$action = ( ! is_admin() && is_admin_bar_showing() )
+			? 'wp_enqueue_scripts'
+			: 'admin_enqueue_scripts';
+		add_action( $action, array( $this, 'embed_admin_assets' ), 101 );
 
 		// Add 'Clear Redis cache' button to the admin bar
 		add_action( 'admin_bar_menu', array( $this, 'add_admin_bar_button' ), 101 );
@@ -35,7 +34,7 @@ class PageSpeedPurge {
 
 	public function embed_admin_assets() {
 		// This is where you can add your CSS/JS entries for wp-admin UI
-		$plugin_url = plugin_dir_url( __FILE__ );
+		$plugin_url   = plugin_dir_url( __FILE__ );
 		$asset_suffix = ( defined( SCRIPT_DEBUG ) && ! empty( SCRIPT_DEBUG ) ) ? '.min' : '';
 		wp_enqueue_style( 'admin-styles', sprintf( '%s/assets/wp-purge-pagespeed-button%s.css', $plugin_url, $asset_suffix ) );
 		wp_enqueue_script( 'admin-styles', sprintf( '%s/assets/wp-purge-pagespeed-button%s.js', $plugin_url, $asset_suffix ), array( 'jquery' ) );
@@ -43,18 +42,18 @@ class PageSpeedPurge {
 
 	public function add_admin_bar_button( $wp_admin_bar ) {
 		$args = [
-			'id'	=> 'pagespeed_purge',
-			'href'	=> '#pagespeed_purge',
-			'title'	=> __( 'Purge Pagespeed Cache', 'wp-purge-pagespeed-button' ),
+			'id'    => 'pagespeed_purge',
+			'href'  => '#pagespeed_purge',
+			'title' => __( 'Purge Pagespeed Cache', 'wp-purge-pagespeed-button' ),
 		];
 
 		$wp_admin_bar->add_menu( $args );
 	}
 
 	public function load_plugin_textdomain() {
-		$textdomain = 'wp-purge-pagespeed-button';
+		$textdomain    = 'wp-purge-pagespeed-button';
 		$plugin_locale = apply_filters( 'plugin_locale', get_locale(), $textdomain );
-		$plugin_dir = dirname( __FILE__ );
+		$plugin_dir    = dirname( __FILE__ );
 
 		// wp-content/languages/plugin-name/plugin-name-ru_RU.mo
 		load_textdomain( $textdomain, sprintf( '%s/plugins/%s-%s.mo', WP_LANG_DIR, $textdomain, $plugin_locale ) );
