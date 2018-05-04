0c5126fb66c7b6b400b5a4f69777b7df0cdf9732
salaros <salaros@yandex.ru>
2018-05-04 22:45:38 +0300

    try to update SVN trunk along with SVN tags upon tagged Travis CI builds

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ if [[ -z "${SVN_REPO_SLUG}" ]] ; then
     exit 1;
 fi
 
-# Go out if Travis CI Git tag-related 
+# Go out if Travis CI Git tag-related
 # environment variable is not set
 if [ -z ${TRAVIS_TAG} ]; then
     echo "Skipping SVN tag creation since it's not a tag-tiggered build"
@@ -20,13 +20,23 @@ fi
 BUILD_DIR=${TRAVIS_BUILD_DIR}/svn
 SVN_ROOT_DIR=${BUILD_DIR}/$(basename ${SVN_REPO_SLUG})
 SVN_TAG_DIR=${SVN_ROOT_DIR}/tags/${TRAVIS_TAG}
-
-# Copy SVN trunk to a tag and commit it
-rsync -av --checksum --delete ${SVN_ROOT_DIR}/trunk/./ ${SVN_TAG_DIR}
-cd ${SVN_TAG_DIR}
+SVN_TRUNK_DIR=${SVN_ROOT_DIR}/trunk
 
 # Bump plugin version
 make plugin_ver
+
+# ================  SVN TRUNK  ======++=========
+# Add stuff to SVN to ignore list
+svn -q propset -R svn:ignore -F ${TRAVIS_BUILD_DIR}/.svnignore ${SVN_TRUNK_DIR} # 2>/dev/null
+
+# Add stuff to SVN and commit files
+svn add --force ${SVN_TAG_DIR}/ # 2>/dev/null
+svn commit -m "bumping plugin version to ${TRAVIS_TAG}" --username ${SVN_USER} --password ${SVN_PASS} --non-interactive --no-auth-cache # 2>/dev/null
+
+# ==================  SVN TAG  ==================
+# Copy SVN trunk to a tag and commit it
+rsync -av --checksum --delete ${SVN_TRUNK_DIR}/./ ${SVN_TAG_DIR}
+cd ${SVN_TAG_DIR}
 
 # Add stuff to SVN to ignore list
 svn -q propset -R svn:ignore -F ${TRAVIS_BUILD_DIR}/.svnignore ${SVN_TAG_DIR} # 2>/dev/null
